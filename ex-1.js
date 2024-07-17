c1d7bc2bedb32c8b31801b430f5fe45ef16084f9
trainthunder <kitikun.po@gmail.com>
2024-07-17 14:32:01 +0700

    feat: algorithm problem solving 2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,54 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let result = -1;
+  for (let i = 0; i < students.length; i++) {
+    // console.log(students[i]);
+    if (searchStudent === students[i]) {
+      result += i + 1;
+    }
+  }
+  return result;
 }
-
+console.log(
+  findStudentIndex(
+    [
+      "Andrew",
+      "Bobby",
+      "Diana",
+      "George",
+      "Hannah",
+      "Isaac",
+      "Jasmine",
+      "John",
+    ],
+    "John"
+  )
+);
+console.log(
+  findStudentIndex(
+    ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"],
+    "Andrew"
+  )
+);
+console.log(
+  findStudentIndex(
+    [
+      "Andrew",
+      "Bobby",
+      "Diana",
+      "George",
+      "Hannah",
+      "Isaac",
+      "Jasmine",
+      "John",
+    ],
+    "Diana"
+  )
+);
 // ตอบคำถามตรงนี้จ้า
+/**
+ * Binary Search จะมี Big O เป็น O(log n)
+ * แต่ในการทำโจทย์นี้ผมยังจำแพทเทรินของ Binary Search ไม่ได้ครับ
+ * เลยทำ Linear Search ไปแทนเพื่อหาคำตอบ
+ * ถ้า Linear Search จะมี Big O เป็น O(n) ซึ่งจะเป็นการไล่เช็คแต่ละ index จากซ้ายไปขวา ทีละตัว
+ */
